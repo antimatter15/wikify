7c7999b4fb027bdb1b0b181651e59387b2b91c81
antimatter15 <antimatter15@d89686f0-7c31-11dd-94ec-bd7bceabbfa5>
2008-09-18 02:33:35 +0000

    updates

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,4 +1,6 @@
 javascript:(function(e){window.WKConf={/*CONFIG HERE*/};e=document.createElement("script");e.src=WKConf.coreurl;document.getElementsByTagName("head")[0].appendChild(e)})()
 
 
-javascript:(function(e){window.WKConf={coreurl:"http://localhost/Wikify/core.js",saveurl:"http://localhost:8080/server/save",loadurl:"http://localhost:8080/server/load",channel:"Main"};e=document.createElement("script");e.src=WKConf.coreurl;document.getElementsByTagName("head")[0].appendChild(e)})()
+javascript:(function(e){window.WKConf={coreurl:"http://localhost/Wikify/core.js",saveurl:"http://wikify.antimatter15.com/server/save",loadurl:"http://wikify.antimatter15.com/server/load",channel:"Main"};e=document.createElement("script");e.src=WKConf.coreurl;document.getElementsByTagName("head")[0].appendChild(e)})()
+
+javascript:(function(e){window.WKConf={coreurl:"http://wikify.antimatter15.com/static2/core.js",saveurl:"http://wikify.antimatter15.com/server/save",loadurl:"http://wikify.antimatter15.com/server/load",channel:"Main"};e=document.createElement("script");e.src=WKConf.coreurl;document.getElementsByTagName("head")[0].appendChild(e)})()
