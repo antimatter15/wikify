480b0ac8c252d5757c6abe4bfe64e931dcaafa47
antimatter15 <antimatter15@d89686f0-7c31-11dd-94ec-bd7bceabbfa5>
2008-11-24 01:23:08 +0000

    Project Wikify v2 (working version, not final)

---
+++ trunk/v2/js/communication.js
@@ -0,0 +1,47 @@
+/*
+saveurl:"http://wikify.antimatter15.com/server/save",
+loadurl:"http://wikify.antimatter15.com/server/load",
+*/
+
+function send_data(url, params, callback){
+  var id = "wk_xfnx"+Math.floor(Math.random()*12345)
+  var div = document.createElement("div");
+  var form = document.createElement("form");
+
+  document.body.appendChild(div);  
+  
+  window[id] = function(){
+    callback();
+    delete window[id];
+    setTimeout(function(){
+      if(div.parentNode){
+        div.parentNode.removeChild(div);
+      }
+    }, 50000)
+  };
+  
+  div.style.display = "none";
+  
+  div.innerHTML = "<iframe id=\""+id+"\" name=\""+id+"\" onload=\"window['"+id+"']()\"></iframe>"
+  
+  form.target = id;
+  form.action = url;
+  form.method = "POST";
+  
+  div.appendChild(form);  
+  
+  for(var key in params){
+    var input = document.createElement("input"); //create new input
+    input.type = "hidden"; //it's hidden! SSH!!!!
+    input.name = key; //set name
+    input.value = params[key]; //set value
+    form.appendChild(input);
+  }
+  
+  form.submit();
+}
+
+
+function get_data(url, params, callback){
+  $.get(url, params, callback, "jsonp")
+}
