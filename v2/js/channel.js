d291aae37456de76dc23548ddc75d6f411a99419
antimatter15 <antimatter15@d89686f0-7c31-11dd-94ec-bd7bceabbfa5>
2008-11-27 18:27:58 +0000

    split into channels

---
+++ v2/js/channel.js
@@ -0,0 +1,37 @@
+
+function wk_set_channel(channel){
+  wk_channel = channel;
+  if(!wk_channels[wk_channel]){
+    wk_channels[wk_channel] = {edits: 0};
+  }
+  $([".wk_btn_original",".wk_btn_view",".wk_btn_edit"][wk_mode]).click()
+  wk_get_channels()
+}
+
+function wk_render_channels(){
+  $("#wk_channel_text").text(wk_channel+" ("+wk_channels[wk_channel].edits+")");
+  $(".wk_chan").remove();
+  for(var i in wk_channels){
+    $("<li></li>")
+    .text(i+" ("+wk_channels[i].edits+")")
+    .addClass("wk_chan")
+    .data("chan",i)
+    .insertBefore(".wk_custom")
+  }
+  
+  $(".wk_chan").click(function(){
+    $(".wk_down").slideUp();
+    wk_set_channel($(this).data("chan"))
+  })
+
+}
+
+function wk_get_channels(callback){
+  wk_get_data(wk_server, {url: "_WikifyTesting"}, function(e){
+    wk_log("Got Channel Data", e)
+    for(var x in e.channels){
+      wk_channels[x] = e.channels[x]
+    }
+    wk_render_channels();
+  })
+}
