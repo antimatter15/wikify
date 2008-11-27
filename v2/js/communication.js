99cca55b5907c6a94454cf2523d3d8c786715dd4
antimatter15 <antimatter15@d89686f0-7c31-11dd-94ec-bd7bceabbfa5>
2008-11-27 20:03:10 +0000

    new *very* jqueryish wk_send_data()

--- v2/js/communication.js
+++ v2/js/communication.js
@@ -5,39 +5,33 @@ loadurl:"http://wikify.antimatter15.com/server/load",
 
 function wk_send_data(url, params, callback){
   var id = "wk_xfnx"+Math.floor(Math.random()*12345)
-  var div = document.createElement("div");
-  var form = document.createElement("form");
 
-  document.body.appendChild(div);  
-  
-  window[id] = function(){
-    callback();
-    delete window[id];
-    setTimeout(function(){
-      if(div.parentNode){
-        div.parentNode.removeChild(div);
-      }
-    }, 50000)
-  };
-  
-  div.style.display = "none";
-  
-  div.innerHTML = "<iframe id=\""+id+"\" name=\""+id+"\" onload=\"window['"+id+"']()\"></iframe>"
-  
-  form.target = id;
-  form.action = url;
-  form.method = "POST";
-  
-  div.appendChild(form);  
-  
-  for(var key in params){
-    var input = document.createElement("input"); //create new input
-    input.type = "hidden"; //it's hidden! SSH!!!!
-    input.name = key; //set name
-    input.value = params[key]; //set value
-    form.appendChild(input);
-  }
-  
+  var form = $("<form>")
+    .addClass(id)
+    .attr({
+      action: url,
+      target: id,
+      method: "POST"
+    })
+    .appendTo("body")
+    .append($("<iframe>")
+      .attr('name',id)
+      .load(function(){
+        callback()
+        setTimeout(function(){
+          form.remove()
+        },5000)
+      }))
+    
+  $.each(params,function(key,value){
+    $("<input>")
+      .attr({
+        type: "hidden",
+        name: key
+      })
+      .val(value)
+      .appendTo(form)
+  })
   form.submit();
 }
 
