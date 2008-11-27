9fcf0967823ede9dc524577e3bdcc6eb7f8241bf
antimatter15 <antimatter15@d89686f0-7c31-11dd-94ec-bd7bceabbfa5>
2008-11-27 22:18:21 +0000

    some stuffs

---
+++ v2/etc/compile_js.php
@@ -0,0 +1,19 @@
+<?php
+$scripts = ["js/jquery.color.js",
+"js/jquery.designmode.js",
+"js/misc.js",
+"js/editor.js",
+"js/mode.js",
+"js/button.js",
+"js/ui.js",
+"js/help.js",
+"js/channel.js",
+"js/data.js",
+"js/communication.js",
+"js/dom.js",
+"js/standalone.js"];
+
+foreach($scripts as $script){
+  echo file_get_contents("../".$script)."\n\n";
+}
+?>
--- v2/js/loader.js
+++ v2/js/loader.js
@@ -2,7 +2,7 @@
   if(window.wk_onlaunch) return wk_onlaunch();
   if(!window.wk_tl) window.wk_tl = [
     "http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js",
-    "http://localhost/Wikify/"
+    "http://localhost/Wikify/v2/etc/compile_js.php"
   ];
   if(wk_tl[0]){
     var s = document.createElement("script");
