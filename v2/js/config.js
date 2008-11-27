9ce11ef0869421cad1db6f04428febd5bce5de2c
antimatter15 <antimatter15@d89686f0-7c31-11dd-94ec-bd7bceabbfa5>
2008-11-27 22:44:20 +0000

    added stuffs

--- v2/js/config.js
+++ v2/js/config.js
@@ -3,8 +3,8 @@ var wk_server = "http://wikify.appjet.net/";
 var wk_original_data = "";
 var wk_channel = "Spam";
 var wk_url = "_WikifyTesting";
+
 var wk_channels = {
   "Spam": {edits: 0},
   "Update": {edits: 0}
 };
-
--- v2/js/init.js
+++ v2/js/init.js
@@ -3,3 +3,10 @@ var wk_readyqueue = [];
 function wk_ready(fn){
   wk_readyqueue.push(fn)
 }
+
+wk_original_data = "<html>"+document.getElementsByTagName("HTML")[0].innerHTML+"</html>";
+
+for(var s=document.styleSheets, i=s.length;i--;){
+  s[i].disabled = true;
+}
+
