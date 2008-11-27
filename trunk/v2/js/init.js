9bfc12ec86808f64632146a136d8943e19e2db2c
antimatter15 <antimatter15@d89686f0-7c31-11dd-94ec-bd7bceabbfa5>
2008-11-27 22:57:30 +0000

    did something adds styles!

--- trunk/v2/js/init.js
+++ trunk/v2/js/init.js
@@ -1,9 +1,13 @@
-
-wk_original_data = "<html>"+document.getElementsByTagName("HTML")[0].innerHTML+"</html>";
-
 (function(){
+  wk_original_data = "<html>"+document.getElementsByTagName("HTML")[0].innerHTML+"</html>";
+
   for(var s=document.styleSheets, i=s.length;i--;){
     s[i].disabled = true;
   }
+  
+  $('<link rel="stylesheet" type="text/css" media="screen">')
+    .attr("href", )
+    .appendTo("head");
+  
+  document.body.innerHTML = wk_toolbar;
 })()
-
