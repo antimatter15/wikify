5ddbaaefd6358ff459a690a4f2356e218150458d
antimatter15 <antimatter15@d89686f0-7c31-11dd-94ec-bd7bceabbfa5>
2008-11-27 18:22:56 +0000

    split off into buttons! faster savings!

--- trunk/v2/js/ui.js
+++ trunk/v2/js/ui.js
@@ -25,85 +25,13 @@ function wk_expand(){
   })
 }
 
-$(document).ready(function(){
-  $(window).resize(function(){
-    if($("#wk_iframe").css("top") == "30px"){
-      $("#wk_iframe").height($(window).height()-30)
-    }else{
-      $("#wk_iframe").height($(window).height())
-    }
-  });
-  
-  $(window).resize();
-  
-  $("#wk_logo").click(wk_collapse)
-  
-  $("#wk_expand").click(wk_expand)
-  
-  $(".wk_mode").click(function(){
-    $(".wk_mode").animate({color: "#858585"})
-    wk_mask(true)
-    $(this).animate({color: "white"})
-  })
-  
-  /*mode buttons*/
-  $(".wk_btn_original").click(wk_original)
-  $(".wk_btn_view").click(wk_view)
-  $(".wk_btn_edit").click(wk_edit)  
-  
-  $(".wk_btn_save").click(function(){
-    wk_saving(true);
-    wk_diffsave(function(){
-        setTimeout(function(){
-          wk_saving(false);
-        },500);
-      }
-    )
-  })
-  
-  $(".wk_btn_news").click(function(){
-    $("#wk_help").slideUp().queue(function(){
-      $("#wk_news").slideToggle();
-      $(this).dequeue();
-    })
-  })
-  
-  $(".wk_btn_help").click(function(){
-    $("#wk_news").slideUp().queue(function(){
-      $("#wk_help").slideToggle();
-      $(this).dequeue();
-    })
-  })
-
-  $("#wk_channel_visible").click(function(){
-    $(".wk_down").slideToggle();
-    $("#wk_news").slideUp()
-    $("#wk_help").slideUp()
-  })
-
-  
-  $("#wk_channel").hover(function(e){
-  },function(e){
-    if($(".wk_down").queue().length == 0){
-      setTimeout(function(){
-        $(".wk_down").slideUp();
-      },500);
-    }
-  })
-  
-  $(".wk_custom").click(function(){
-    var newchan = prompt("Enter name of channel you would like to go to or create.");
-    if(newchan){
-      $(".wk_down").slideUp();
-      wk_set_channel(newchan)
-    }else{
-      //do nothing
-    }
-  })
-  
-  //$([".wk_btn_original",".wk_btn_view",".wk_btn_edit"][wk_mode]).click()
-  
-})
+function wk_resize(){
+  if($("#wk_iframe").css("top") == "30px"){
+    $("#wk_iframe").height($(window).height()-30)
+  }else{
+    $("#wk_iframe").height($(window).height())
+  }
+}
 
 
 function wk_saving(mode){
@@ -125,3 +53,11 @@ function wk_mask(mode){
     $("#wk_mask").slideUp()
   }
 }
+
+$(document).ready(function(){
+  $(window).resize(wk_resize);
+  wk_resize();
+  
+  $("#wk_logo").click(wk_collapse)
+  $("#wk_expand").click(wk_expand)
+})
