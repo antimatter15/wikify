37be8784c4f77b0510cabbc7932698aa59239f21
antimatter15 <antimatter15@d89686f0-7c31-11dd-94ec-bd7bceabbfa5>
2008-11-27 19:29:37 +0000

    fixed compilier! compiles into js/toolbar.js

--- v2/compile.php
+++ v2/compile.php
@@ -1,5 +1,5 @@
 <?php
-
+header("content-type: text/javascript");
   
   //from http://www.justin-cook.com/wp/2006/03/31/php-parse-a-string-between-two-strings/
 function get_string_between($string, $start, $end){
@@ -19,7 +19,9 @@ $html = get_string_between(file_get_contents("toolbar.htm"),
 //http://www.blog.highub.com/regular-expression/php-regex-regular-expression/php-regex-remove-whitespace-from-html/
 $newhtml = preg_replace( "/(?:(?<=\>)|(?<=\/\>))(\s+)(?=\<\/?)/","", $html);
 
-//$newhtml = str_replace("\n","",$newhtml);
+$newhtml = preg_replace("/[\n\r]/","",$newhtml);
 
-return $newhtml;
+$out =  "var wk_toolbar = '".$newhtml."';";
+
+file_put_contents("js/toolbar.js",$out);
 ?>
