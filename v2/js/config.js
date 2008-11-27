82335776f1511de6ba213139f30d09875928b012
antimatter15 <antimatter15@d89686f0-7c31-11dd-94ec-bd7bceabbfa5>
2008-11-27 23:08:08 +0000

    almost worksish!

--- v2/js/config.js
+++ v2/js/config.js
@@ -3,11 +3,15 @@ var wk_server = "http://wikify.appjet.net/";
 var wk_original_data = "";
 var wk_channel = "Spam";
 var wk_url = "_WikifyTesting";
-
+var wk_style = "http://localhost/Wikify/v2/styles.css"
+var wk_img = "http://localhost/Wikify/v2/img/"
 var wk_channels = {
   "Spam": {edits: 0},
   "Update": {edits: 0}
 };
+
+
+/*Not-Really variables*/
 
 var wk_readyqueue = [];
 
