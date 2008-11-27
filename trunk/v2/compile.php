1812cc125c867388fdddb7d96d8a5d5be36ae793
antimatter15 <antimatter15@d89686f0-7c31-11dd-94ec-bd7bceabbfa5>
2008-11-27 19:00:02 +0000

    bettr-compilier

--- trunk/v2/compile.php
+++ trunk/v2/compile.php
@@ -1,5 +1,23 @@
 <?php
-$html = file_get_contents("toolbar.htm");
+
+  
+  //from http://www.justin-cook.com/wp/2006/03/31/php-parse-a-string-between-two-strings/
+function get_string_between($string, $start, $end){
+        $string = " ".$string;
+        $ini = strpos($string,$start);
+        if ($ini == 0) return "";
+        $ini += strlen($start);
+        $len = strpos($string,$end,$ini) - $ini;
+        return substr($string,$ini,$len);
+}
+
+$html = get_string_between(file_get_contents("toolbar.htm"),
+              "<!--BEGIN Main-->",
+              "<!--END Main-->");
+
+
+//http://www.blog.highub.com/regular-expression/php-regex-regular-expression/php-regex-remove-whitespace-from-html/
 echo preg_replace( "/(?:(?<=\>)|(?<=\/\>))(\s+)(?=\<\/?)/","", $html )
-                    
+  
+
 ?>
