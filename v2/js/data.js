e1de37c1657d86331d27f155d3b768124bf15d5b
antimatter15 <antimatter15@d89686f0-7c31-11dd-94ec-bd7bceabbfa5>
2008-11-27 01:45:08 +0000

    get_channels

--- v2/js/data.js
+++ v2/js/data.js
@@ -1,9 +1,15 @@
 var server = "http://wikify.appjet.net/";
 var original_data = "";
+var wk_channel = "Spam";
+var wk_channels = {
+"Spam": {edits: 0},
+"Update": {edits: 0}
+};
 
 $.get("content.htm",{}, function(e){
   original_data = e;
   $(document).ready(function(){
+  get_channels()
     $([".wk_btn_original",".wk_btn_view",".wk_btn_edit"][wk_mode]).click()
   })
 })
@@ -17,7 +23,7 @@ function write_data(data){
   $(doc).find("a") //find all links
     .click(function(){ //on click event
       window.parent.location = this.href; //make them open up in the parent
-    })
+  })
 }
 
 function write_original(){
@@ -25,8 +31,28 @@ function write_original(){
 }
 
 
+function render_channels(){
+  $("#wk_channel_text").text(wk_channel+" ("+wk_channels[wk_channel].edits+")");
+  $(".wk_chan").remove();
+  for(var i in wk_channels){
+    $("<li></li>")
+    .text(i+" ("+wk_channels[i].edits+")")
+    .addClass("wk_chan")
+    .insertBefore(".wk_custom")
+  }
+}
+
+function get_channels(callback){
+  get_data(server, {url: "_WikifyTesting"}, function(e){
+    for(var x in e.channels){
+      wk_channels[x] = e.channels[x]
+    }
+    render_channels();
+  })
+}
+
 function load(callback){
-  get_data(server, {url: "_WikifyTesting", channel: "_WikifyTesting"}, 
+  get_data(server, {url: "_WikifyTesting", channel: wk_channel}, 
     function(data){
       var edits = [];
       for(var i = 0; i < data.edits.length; i++){
@@ -34,27 +60,21 @@ function load(callback){
       }
       parse(edits);
       wk_log("Loaded Data: ",data)
-      //console.log(data)
       if(callback) callback();
     })
-  
 }
 
 function save(callback){
-  if(!snapshot){
-    wk_log("Error: No Snapshot!")
-  }
-
-  var changes = diff();
+  if(!snapshot){wk_log("Error: No Snapshot!")}
   
+  var changes = diff();
   if(changes == "" || wk_mode != 2) return callback?callback():false; //no need for simple edits
   
-  //console.log(changes)
-  
-  send_data(server, {url: "_WikifyTesting", channel: "_WikifyTesting", data: changes}, 
+  send_data(server, {url: "_WikifyTesting", channel: wk_channel, data: changes}, 
     function(){
       if(callback) callback();
       wk_log("Sent Data: ",changes)
     })
     snapshot = capture();
+    get_channels()
 }
