3f9bdbeaefdcfd33d16f0f0711a03e7ede684090
antimatter15 <antimatter15@d89686f0-7c31-11dd-94ec-bd7bceabbfa5>
2008-11-27 00:57:25 +0000

    purty help

--- trunk/v2/js/help.js
+++ trunk/v2/js/help.js
@@ -1,29 +1,28 @@
 $(document).ready(function(){
   var areas = {
-    "#wk_logo": "Click on this to minimize the toolbar",
+    "#wk_logo": "Click on this logo to minimize the toolbar",
+    ".wk_btn_save": "Save your edits to the server",
     ".wk_btn_news": "Find the latest Wikify edits",
-    ".wk_btn_help": "Gets you here!",
+    ".wk_btn_help": "Gets you here! (Awesome Help)",
     ".wk_btn_original": "View the original, unmodified page",
     ".wk_btn_view": "View the page, with the Wikify changes",
-    ".wk_btn_edit": "Edit the page like a word processor document",
+    ".wk_btn_edit": "Edit the page like a word processor",
     "#wk_channel_visible": "Switch Wikify changesets."
   }
   
   $.each(areas,function(key, value){
-    console.log(key, value)
     $(key).mouseover(function(){
-  
-      $("wk_tooltip")
-        .fadeOut()
+      $("#wk_tooltip")
+        .stop(true)
         .animate({
-          marginLeft: $(key).offset().left
-        },"slow","swing")
+          marginLeft: ($(key).offset().left+$(key).width()+50)>$(document).width()?
+                      $(key).offset().left-75:
+                      $(key).offset().left
+        },"fast","swing")
         .queue(function(){
           $(this).text(value)
           $(this).dequeue();
         })
-        .fadeIn();
-      
     })
   })
 })
