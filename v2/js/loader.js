759392f57cc65e7072b1f57412167193297c236a
antimatter15 <antimatter15@d89686f0-7c31-11dd-94ec-bd7bceabbfa5>
2008-11-27 22:12:50 +0000

    new js loader! (bookmarklet)

--- v2/js/loader.js
+++ v2/js/loader.js
@@ -1,10 +1,14 @@
-var wk_jQuery = "http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js";
-
-function wk_loadscript(src, callback){
-  var s = document.creatElement("script");
-  s.type = 'text/javascript';s.src = src;
-  s.onreadystatechange = s.onload = callback;
-  document.getElementsByTagName("head")[0].appendChild(s)
-}
-
-wk_loadscript(wk_jQuery, )
+(function(){
+  if(window.wk_onlaunch) return wk_onlaunch();
+  if(!window.wk_tl) window.wk_tl = [
+    "http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js",
+    "http://localhost/Wikify/"
+  ];
+  if(wk_tl[0]){
+    var s = document.createElement("script");
+    s.type = 'text/javascript';
+    s.src = wk_tl.splice(0,1)[0];
+    s.onreadystatechange = s.onload = arguments.callee;
+    document.getElementsByTagName("head")[0].appendChild(s);
+  }
+})()
