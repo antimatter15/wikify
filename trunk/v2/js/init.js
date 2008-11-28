00e8d9bd39bc5cbb2f5df1d6d8a6ea622eafa71d
antimatter15 <antimatter15@d89686f0-7c31-11dd-94ec-bd7bceabbfa5>
2008-11-28 17:02:24 +0000

    basic error detection

--- trunk/v2/js/init.js
+++ trunk/v2/js/init.js
@@ -1,11 +1,14 @@
 (function(){
+  if(!$("html")[0] || !$("body")[0] || !$("head")[0]){
+    return alert("Project Wikify has encountered a fatal error\n(Missing Required Document Elements)")
+  }
   if(wk_removescripts){
     //$("script").remove(); //remove all scripts (strangely doesn't work on ajaxian)
     $(document.getElementsByTagName("script")).remove()
   }else{
     $("script.wk_initsc").remove(); //remove the loader scripts
   }
-  wk_original_data = "<html>"+document.getElementsByTagName("HTML")[0].innerHTML+"</html>";
+  wk_original_data = "<html>"+$("html").html()+"</html>";
   
   for(var s=document.styleSheets, i=s.length;i--;){ //loop through styles
     s[i].disabled = true; //and kill them
@@ -18,8 +21,10 @@
     .attr("href", wk_style)
     .appendTo("head");
 
+  if(!$("#wk_iframe")[0] || !$("#wk_toolbar")[0]){
+    return alert("Project Wikify has encountered a fatal error\n(Missing Generated Elements)")
+  }
 
-  
   for(var i = 0; i < wk_readyqueue.length; i++){
     wk_readyqueue[i](); //run readyqueue
   }
