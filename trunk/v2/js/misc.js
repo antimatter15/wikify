85c733ded7ca9653464ccac3755bf6a66bff3ff9
antimatter15 <antimatter15@d89686f0-7c31-11dd-94ec-bd7bceabbfa5>
2008-11-28 00:54:01 +0000

    um... i'll blame this on microsoft.

--- trunk/v2/js/misc.js
+++ trunk/v2/js/misc.js
@@ -1,15 +1,19 @@
-if(window.console && !$.browser.safari){
-  console.log();
+if(window.console){
+  try{
+    console.log();
+  }catch(err){}
 }
 
 function wk_log(){
-  //because console.log.apply only works on firefox :(
-  var a = arguments, l = a.length;
-  if(l == 42) console.log("TEH WORLDZ ASPLODE!")
-  else if(l == 1) console.log(a[0])
-  else if(l == 2) console.log(a[0],a[1])
-  else if(l == 3) console.log(a[0],a[1],a[2])
-  else if(l == 4) console.log(a[0],a[1],a[2],a[3]);
+  if(window.console){
+    //because console.log.apply only works on firefox :(
+    var a = arguments, l = a.length;
+    if(l == 42) console.log("TEH WORLDZ ASPLODE!")
+    else if(l == 1) console.log(a[0])
+    else if(l == 2) console.log(a[0],a[1])
+    else if(l == 3) console.log(a[0],a[1],a[2])
+    else if(l == 4) console.log(a[0],a[1],a[2],a[3]);
+  }
 }
 
 function wk_ready(fn){
