4ca315a9953dba0fb5ba79c0d680169ba959b1e3
antimatter15 <antimatter15@d89686f0-7c31-11dd-94ec-bd7bceabbfa5>
2008-11-28 00:23:55 +0000

    settled for the microsoft approach to doing .apply http://ajaxian.com/archives/when-generated-javascript-goes-wild-javascript-wtf-in-sharepoint

--- trunk/v2/js/misc.js
+++ trunk/v2/js/misc.js
@@ -2,10 +2,8 @@ if(window.console && !$.browser.safari){
   console.log();
 }
 
-function wk_log(){
-  if(window.console && !$.browser.safari){
-    console.log.apply(this,arguments);
-  }
+function wk_log(a,b,c,d,e,f,g){
+  console.log(a,b,c,d,e,f,g); //because console.log.apply only works on firefox :(
 }
 
 function wk_ready(fn){
