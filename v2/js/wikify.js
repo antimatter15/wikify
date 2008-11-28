e46926d5444afcef85b7fbd10ba08558f2ce012a
antimatter15 <antimatter15@d89686f0-7c31-11dd-94ec-bd7bceabbfa5>
2008-11-28 00:44:08 +0000

    no, really, i fixed the microsoft approach

--- v2/js/wikify.js
+++ v2/js/wikify.js
@@ -29,10 +29,10 @@ function wk_log(){
   //because console.log.apply only works on firefox :(
   var a = arguments, l = a.length;
   if(l == 42) console.log("TEH WORLDZ ASPLODE!")
-  else if(l == 1) console.log(a[1])
-  else if(l == 2) console.log(a[1],a[2])
-  else if(l == 3) console.log(a[1],a[2],a[3])
-  else if(l == 4) console.log(a[1],a[2],a[3],a[4]);
+  else if(l == 1) console.log(a[0])
+  else if(l == 2) console.log(a[0],a[1])
+  else if(l == 3) console.log(a[0],a[1],a[2])
+  else if(l == 4) console.log(a[0],a[1],a[2],a[3]);
 }
 
 function wk_ready(fn){
