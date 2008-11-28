952ec5f9b77b939f3e68a8c1e44e6c845445149a
antimatter15 <antimatter15@d89686f0-7c31-11dd-94ec-bd7bceabbfa5>
2008-11-28 00:12:22 +0000

    mini works!

--- trunk/v2/js/jquery.color.mini.js
+++ trunk/v2/js/jquery.color.mini.js
@@ -9,17 +9,17 @@
 (function(jQuery){
 
 	// We override the animation for all of these color styles
-	jQuery.each(['backgroundColor', 'borderBottomColor', 'borderLeftColor', 'borderRightColor', 'borderTopColor', 'color', 'outlineColor'], function(i,attr){
+	jQuery.each(['backgroundColor', 'color'], function(i,attr){
 		jQuery.fx.step[attr] = function(fx){
 			if ( fx.state == 0 ) {
-				fx.start = getColor( fx.elem, attr );
+				fx.start = getRGB(jQuery.curCSS(fx.elem, attr));
 				fx.end = getRGB( fx.end );
 			}
 
 			fx.elem.style[attr] = "rgb(" + [
-				Math.max(Math.min( parseInt((fx.pos * (fx.end[0] - fx.start[0])) + fx.start[0]), 255), 0),
-				Math.max(Math.min( parseInt((fx.pos * (fx.end[1] - fx.start[1])) + fx.start[1]), 255), 0),
-				Math.max(Math.min( parseInt((fx.pos * (fx.end[2] - fx.start[2])) + fx.start[2]), 255), 0)
+				parseInt((fx.pos * (fx.end[0] - fx.start[0])) + fx.start[0]),
+				parseInt((fx.pos * (fx.end[1] - fx.start[1])) + fx.start[1]),
+				parseInt((fx.pos * (fx.end[2] - fx.start[2])) + fx.start[2])
 			].join(",") + ")";
 		}
 	});
@@ -31,13 +31,13 @@
 	// Parse strings looking for color tuples [255,255,255]
 	function getRGB(color) {
 		var result;
-		
+	  // Look for rgb(num,num,num)
+		if (result = /rgb\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*\)/.exec(color))
+			return [parseInt(result[1]), parseInt(result[2]), parseInt(result[3])];
+
 		// Look for #a0b1c2
 		if (result = /#([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})/.exec(color))
 			return [parseInt(result[1],16), parseInt(result[2],16), parseInt(result[3],16)];
+			
 	}
-	
-	function getColor(elem, attr) {
-		return getRGB(jQuery.curCSS(elem, attr));
-	};
 })(jQuery);
