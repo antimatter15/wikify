effa61fc134e441956800ae930b5c74e6910a27e
antimatter15 <antimatter15@d89686f0-7c31-11dd-94ec-bd7bceabbfa5>
2008-11-28 00:39:53 +0000

    oh great. i have to use the *real* microsoft approach :(

--- trunk/v2/js/misc.js
+++ trunk/v2/js/misc.js
@@ -4,11 +4,12 @@ if(window.console && !$.browser.safari){
 
 function wk_log(){
   //because console.log.apply only works on firefox :(
-  var s = "";
-  for(var i = 0; i < arguments.length; i++){
-    s+="arg["+i+"]"+(i==arguments.length-1?"":",");
-  }
-  eval("(function(arg){console.log("+s+")})")(arguments)
+  var a = arguments, l = a.length;
+  if(l == 42) console.log("TEH WORLDZ ASPLODE!")
+  else if(l == 1) console.log(a[1])
+  else if(l == 2) console.log(a[1],a[2])
+  else if(l == 3) console.log(a[1],a[2],a[3])
+  else if(l == 4) console.log(a[1],a[2],a[3],a[4]);
 }
 
 function wk_ready(fn){
