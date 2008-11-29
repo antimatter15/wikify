1b4470ae95ffc9e7b6057285d6679e0b68d5cf25
antimatter15 <antimatter15@d89686f0-7c31-11dd-94ec-bd7bceabbfa5>
2008-11-29 17:07:53 +0000

    stuff about:blank

--- trunk/v2/js/init.js
+++ trunk/v2/js/init.js
@@ -1,9 +1,7 @@
 (function(){
-  if(!$("html")[0] || !$("body")[0] || !$("head")[0]){
+  if(!$("html")[0] || !$("body")[0]){
     return alert("Project Wikify has encountered a fatal error\n(Missing Required Document Elements)")
   }
-  
-  
   if(wk_removescripts){
     //$("script").remove(); //remove all scripts (strangely doesn't work on ajaxian)
     $(document.getElementsByTagName("script")).remove()
@@ -17,8 +15,7 @@
   for(var s=document.styleSheets, i=s.length;i--;){ //loop through styles
     s[i].disabled = true; //and kill them
   }
-  
-
+ 
   document.body.innerHTML = wk_toolbar.split("img/").join(wk_img)
 
   var m = document.createElement("div");
@@ -26,6 +23,7 @@
   m.setAttribute("style","font-family:Tahoma,Verdana,'Trebuchet MS',Arial,Helvetica,sans-serif;color:#000;background-color:#8095AA;position:absolute;width:100%;height:100%;top:0;left:0;font-size:100px;z-index:51000;text-align:center")
   m.innerHTML="Loading..." 
   document.body.appendChild(m);
+  
 
   $('<link rel="stylesheet" type="text/css" media="screen">') //add the styles
     .attr("href", wk_style)
