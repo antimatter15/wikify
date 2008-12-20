c680d05d1ccad5933b0b67850ecceca8b7c6a07e
antimatter15 <antimatter15@d89686f0-7c31-11dd-94ec-bd7bceabbfa5>
2008-12-20 18:48:11 +0000

    compresses appengine too!

--- v2/etc/compress_js.bat
+++ v2/etc/compress_js.bat
@@ -1,2 +1,3 @@
 java -jar yuicompressor-2.4.2.jar ../js/wikify.js -o ../js/wikify_mini.js
+java -jar yuicompressor-2.4.2.jar ../../appengine/webwikify/static3/wikify2.js -o ../../appengine/webwikify/static3/wikify2.js
 pause
