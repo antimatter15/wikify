fab0da45bbbc342f64648aac46678601f6da3cde
antimatter15 <antimatter15@d89686f0-7c31-11dd-94ec-bd7bceabbfa5>
2008-12-21 16:20:30 +0000

    changed a bit

--- trunk/v2/js/loader.js
+++ trunk/v2/js/loader.js
@@ -1,7 +1,6 @@
-(function(){
-  var w = window;
+(function(w){
   if(document.body.tagName.toLowerCase()!="body"){
-    w = frames['content'];
+    w = frames[1];
   }
   var d=w.document,b=d.body;
   if(w.wk_onlaunch) return wk_onlaunch();
@@ -19,11 +18,11 @@
     m.setAttribute("style","font-family:Tahoma,Verdana,'Trebuchet MS',Arial,Helvetica,sans-serif;color:#000;background-color:#8095AA;position:absolute;width:100%;height:100%;top:0;left:0;font-size:100px;z-index:51000;text-align:center")
     m.innerHTML="Loading..." 
     b.appendChild(m);
-    l("http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js")
+    if(!w.jQuery) l("http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js");
   }
   if(!w.jQuery){
     setTimeout(arguments.callee, 100);
   }else{
     l("http://wikify.antimatter15.com/static3/wikify2.js")
   }
-})()
+})(window)
